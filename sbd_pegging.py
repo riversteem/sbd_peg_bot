e7b3c8c0771e563789ce0e52a724702eae77c164
james <james@james-desktop.home>
2016-07-18 07:33:01 -0400

    Added some failsafe logic for feed price.

--- sbd_pegging.py
+++ sbd_pegging.py
@@ -4,6 +4,7 @@ import json
 import time
 import yaml
 import os
+import sys
 from bittrex.bittrex import Bittrex
 from coinbase.wallet.client import Client
 
@@ -17,10 +18,26 @@ with open("config.yml", "r") as config_file:
   minOrder  = float(config['markets']['sbd_btc']['min_order_size'])
   maxOrder  = float(config['markets']['sbd_btc']['max_order_size'])
   market    = config['markets']['sbd_btc']['market']
+  sbdTarget = float(config['markets']['sbd_btc']['BTCUSD'])
+  priceSafety= float(config['markets']['sbd_btc']['feed_tolorance'])
+  redFlags = 0
 
 while True:
 
+  lastTarget = sbdTarget
   sbdTarget = 1/float(coinbase.get_spot_price(currency='USD')['amount'])
+
+  #Make sure we don't get hosed if feed price slips too much
+  if (abs(lastTarget - sbdTarget)/lastTarget/100) > priceSafety:
+    print("Something off with the price feed, skipping this round\n")
+    redFlags = redFlags + 1
+    continue
+
+  if redFlags >= 5:
+     print("Something is really off! Shutting down.\n")
+     sys.exit()
+
+  lastTarget = sbdTarget
 
   bidRatio = sbdTarget - (sbdTarget * sbdBtcSpread)
   askRatio = sbdTarget + (sbdTarget * sbdBtcSpread)
@@ -46,7 +63,7 @@ while True:
   _=os.system('clear')
 
   print("========================================================================================\n")
-  print("Balances:\nBTC: %f ($%0.2f) SBD: %f\n" % (btcBalAvail, btcBalAvail * 1/sbdTarget, sbdBalAvail))
+  print("RED FLAGS: %0.0f\nBalances:\nBTC: %f ($%0.2f) SBD: %f\n" % (redFlags,btcBalAvail, btcBalAvail * 1/sbdTarget, sbdBalAvail))
   print("Targets:\nBid: %f @ %f - Peg %f - Ask: %f @ %f\n" % (btcBalAvail, bidRatio, sbdTarget, sbdBalAvail, askRatio))
   print("========================================================================================\n")
 
@@ -61,7 +78,7 @@ while True:
     result = bittrex.sell_limit(market, sbdBalAvail, askRatio)
 
 
-  print("Open Orders:")
+  print("Open Orders:\n")
   open_orders = bittrex.get_open_orders(market)['result']
   for order in open_orders:
     ordUid  = order['OrderUuid']
@@ -72,16 +89,16 @@ while True:
     #Cancel out of bound orders - async if possible:
     if ordType == 'LIMIT_BUY':
       if ((ratio * 1.001) < bidRatio) or (ratio > sbdTarget):
-        print("%s %s %s %s [CANCELING BUY]" %  (ordUid, remaining, ordType, ratio))
+        print("%s %s %s %s [CANCELING BUY]\n" %  (ordUid, remaining, ordType, ratio))
         result = bittrex.cancel(ordUid)
       else:
-        print("%s %s %s %s" %  (ordUid, remaining, ordType, ratio))
+        print("%s %s %s %s\n" %  (ordUid, remaining, ordType, ratio))
     else:
       if (ratio > (askRatio * 1.001)) or (ratio < sbdTarget):
-        print("%s %s %s %s [CANCELING SELL]" %  (ordUid, remaining, ordType, ratio))
+        print("%s %s %s %s [CANCELING SELL]\n" %  (ordUid, remaining, ordType, ratio))
         result = bittrex.cancel(ordUid)
       else:
-        print("%s %s %s %s" %  (ordUid, remaining, ordType, ratio))
+        print("%s %s %s %s\n" %  (ordUid, remaining, ordType, ratio))
       
   time.sleep(10)
   
